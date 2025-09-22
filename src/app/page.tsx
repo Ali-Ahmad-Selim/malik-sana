bf361134bb536c4c386bda5f58707cd6edd33a99
Ali ahmad <ali732152ahmad@gmail.com>
2025-09-22 14:52:57 +0500

    structure rebuilding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
- import Secondary from "./secondary/page"
-const Page = () => {
-  return (
-    <div>
-      <Secondary />
-    </div>
-  )
-}
+import { FaWhatsapp } from "react-icons/fa";
+import Link from "next/link";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-export default Page
+const Secondary = () => {
+  return (
+    <div className="relative h-full">
+      <div>
+        <Header/>
+        <Footer/>      
+      </div>
+
+      <Link 
+        href="https://wa.me/+923074081226" // Replace with your WhatsApp number
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-6 right-6 bg-green-500 p-4 rounded-full shadow-lg hover:bg-green-600 transition-colors duration-300 z-50"
+        aria-label="Chat on WhatsApp"
+      >
+        <FaWhatsapp className="text-white text-3xl" />
+      </Link>
+    </div>
+  );
+};
+
+export default Secondary;
